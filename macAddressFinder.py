1ab56d7b5855185c3ce586abc0defde52ecf132b
Juno <astromulk@gmail.com>
2021-08-14 21:06:02 +1000

    added ignore flag

--- macAddressFinder.py
+++ macAddressFinder.py
@@ -37,6 +37,7 @@ def displayHelp():
     print("-c \t--captureFile \tThe path to the Aircrack csv capture file.")
     print("-d \t--database \tThe path to the database of MAC address prefixes.")
     print("\t\tMust be in the format XX:XX:XX, Vendor Name")
+    print("-i \t--ignore \tDo not print (ignore) MAC addresses that return 'No Vendor Found'")
     print("-h \t--help \tDisplay this help")
     exit()
 
@@ -50,7 +51,7 @@ def displaySpinner():
         time.sleep(0.1)
 
 # test the path of the database and open/read file
-def loadDatabase(databasePath):
+def loadDatabase(databasePath, macPrefixes):
     # characters we don't want in our vendor string
     charToOmit = ",\t\n\f\r\""
     # try opening the file
@@ -68,7 +69,7 @@ def loadDatabase(databasePath):
         exit()
 
 # test the path of the capture and open/read file
-def loadCapture(capturePath):
+def loadCapture(capturePath, foundMacs):
     read_data = ""
     # try opening the file
     try: 
@@ -83,64 +84,84 @@ def loadCapture(capturePath):
         foundMacs.add(match[1].lower())
 
 # print the found mac address and their vendors
-def printFoundMacs(foundMacs, printedMacs, macPrefixes):
+def printFoundMacs(foundMacs, printedMacs, macPrefixes, hideNoVendorFound):
     for mac in foundMacs:
         if mac not in printedMacs:
+            # to keep track of when it has been printed in a colour
             needsPrinting = True
+            # add it to the list of printed macs
             printedMacs.append(mac.lower())
+            # get the prefix for the vendor
             macPrefix = mac[:8]
+
+            # assign the vendor from the database. if it's not in the database give it
+            # no vendor found
             vendor = macPrefixes.get(macPrefix.lower(), "No vendor found")
-            for name in vendorsToShowInRed: 
-                if name.lower() in vendor.lower():
-                    print(f"{colorama.Fore.RED}", mac, "\t", vendor, f"{colorama.Style.RESET_ALL}")
-                    needsPrinting = False
-                    break
-            for name in vendorsToShowInBlue:
-                if name.lower() in vendor.lower(): 
-                    print(f"{colorama.Fore.BLUE}", mac, "\t", vendor, f"{colorama.Style.RESET_ALL}")
-                    needsPrinting = False
-                    break
-            for name in vendorsToShowInGreen: 
-                if name.lower() in vendor.lower():
-                    print(f"{colorama.Fore.GREEN}", mac, "\t", vendor, f"{colorama.Style.RESET_ALL}")
-                    needsPrinting = False
-                    break
-            if needsPrinting:
-                print("", mac, "\t", vendor)
+
+            # exit out if do not print no vendor flag has been set
+            if hideNoVendorFound and vendor == "No vendor found":
+                pass
+
+            else:
+                for name in vendorsToShowInRed: 
+                    if name.lower() in vendor.lower():
+                        print(f"{colorama.Fore.RED}", mac, "\t", vendor, f"{colorama.Style.RESET_ALL}")
+                        needsPrinting = False
+                        break
+                for name in vendorsToShowInBlue:
+                    if name.lower() in vendor.lower(): 
+                        print(f"{colorama.Fore.BLUE}", mac, "\t", vendor, f"{colorama.Style.RESET_ALL}")
+                        needsPrinting = False
+                        break
+                for name in vendorsToShowInGreen: 
+                    if name.lower() in vendor.lower():
+                        print(f"{colorama.Fore.GREEN}", mac, "\t", vendor, f"{colorama.Style.RESET_ALL}")
+                        needsPrinting = False
+                        break
+                if needsPrinting:
+                    print("", mac, "\t", vendor)
 
 # Variables
 # put the flags given here
 flags = sys.argv
-# inputs
-database = ""
-capture = ""
-# dictionary to hold our prefixes
-macPrefixes = {}
-# set of found mac addresses
-foundMacs = set()
-# list of printed mac addresses
-printedMacs = []
 
 def main():
+    # Variables    
+    # inputs
+    database = ""
+    capture = ""
+    # dictionary to hold our prefixes
+    macPrefixes = {}
+    # set of found mac addresses
+    foundMacs = set()
+    # list of printed mac addresses
+    printedMacs = []
+    # flag for hiding no vendor found macs
+    hideNoVendorFound = False
+
     # if user has asked for help, or not given any inputs
     if len(flags) == 1 or len(flags) > 1 and flags[1] == "--help" or flags[1] == "-h":
         displayHelp()
 
     # tokenize the relevant inputs
     for index, token in enumerate(flags):
+        # set the database
         if token == '--database' or token == '-d':
             database = flags[index + 1]
+        # set the capture file
         if token == '--captureFile' or token == '-c':
             capture = flags[index + 1]
+        if token == '--ignore' or token == '-i':
+            hideNoVendorFound = True
     # if no database or capture found, display help
     if database == "" or capture == "" :
         displayHelp()
 
     # load the database file
-    loadDatabase(database)
+    loadDatabase(database, macPrefixes)
 
     # load the capture file
-    loadCapture(capture)
+    loadCapture(capture, foundMacs)
 
     # block of code for keyboard intuerrupt
     try: 
@@ -156,8 +177,8 @@ def main():
 
         # infinite loop to print results
         while True:
-            loadCapture(capture)
-            printFoundMacs(foundMacs, printedMacs, macPrefixes)
+            loadCapture(capture, foundMacs)
+            printFoundMacs(foundMacs, printedMacs, macPrefixes, hideNoVendorFound)
             time.sleep(3)
 
     except KeyboardInterrupt:
